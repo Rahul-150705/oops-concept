cb39cf9801d16ca91b9b6f7b0676920555fa8e03
Rahul P <rahul.cd23@bitsathy.ac.in>
2025-08-12 23:41:54 +0530

    Create EmployeeStatus.java

---
+++ day3/EmployeeStatus.java
@@ -0,0 +1,14 @@
+package encapsulationn;
+
+public class EmployeStatus {
+
+	public static void main(String[] args) {
+		Employee detail=new Employee();
+		detail.setName("Rahul");
+		detail.setSalary(500000);
+		detail.setBonus(10000);
+		detail.displayInfo();
+
+	}
+
+}
