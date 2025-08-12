48f018151e863f3e8d6628bba42af71b666aaf7e
Rahul P <rahul.cd23@bitsathy.ac.in>
2025-08-12 23:16:10 +0530

    Create Encapsulation.java

---
+++ day3/Encapsulation.java
@@ -0,0 +1,14 @@
+package encapsulationn;
+
+public class Encapsulationn {
+
+	public static void main(String[] args) {
+		Student details=new Student();
+		details.setName("Rahul");
+		details.Setage(20);
+		details.Setmark(98.5);
+		details.displayInfo();
+
+	}
+
+}
